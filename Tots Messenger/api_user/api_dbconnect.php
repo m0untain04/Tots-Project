3ec87cb3c9bd4d65a9d43da7bcc7cdc299754da0
Alex <38536866+EnderIce2@users.noreply.github.com>
2021-05-27 23:20:46 +0300

    change $conn to $link

--- Tots Messenger/api_user/api_dbconnect.php
+++ Tots Messenger/api_user/api_dbconnect.php
@@ -7,10 +7,10 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'tots_project');
  
 /* Attempt to connect to MySQL database */
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 /* Check connection */
-if ($conn -> connect_errno) {
+if ($link -> connect_errno) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
